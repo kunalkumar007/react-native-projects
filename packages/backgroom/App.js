f04e8e2627f70989cf58834ae91799fd9f98a680
Kunal Kumar <kunakumar06022003@gmail.com>
2021-05-19 19:16:57 +0530

    seekbar js added

--- packages/backgroom/App.js
+++ packages/backgroom/App.js
@@ -3,6 +3,7 @@ import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
 import Header from './components/Header';
 import AlbumArt from './components/AlbumArt';
 import TrackDetails from './components/TrackDetails';
+import SeekBar from './components/seekBar';
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
       <StatusBar barStyle="light-content" />
       <Header message="Playing from charts" />
       <AlbumArt url="https://64.media.tumblr.com/14e9a12cd4dca7a3c3c4fe178b607d27/tumblr_nlott6SmIh1ta3rfmo1_1280.jpg" />
-      <TrackDetails title="Kunal Kuar" artist="kdnjkfjn"  />
+      <TrackDetails title="Stressed Out" artist="Twenty One Pilots" />
+      <SeekBar trackLength={204} currentPosition={156} />
     </SafeAreaView>
   );
 };
