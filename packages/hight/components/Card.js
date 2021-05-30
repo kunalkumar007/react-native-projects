a1abec18c262406c87ec3b031f7fabc44ea296ac
Kunal Kumar <kunakumar06022003@gmail.com>
2021-05-31 01:06:46 +0530

    hight & whist added

---
+++ packages/hight/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function Card() {
+  return (
+    <View>
+      <Text>Card</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ packages/hight/components/Deck.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+export default function Deck() {
+  return (
+    <View>
+      <Text>Deck</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
